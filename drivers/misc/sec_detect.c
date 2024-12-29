9d2f1de7983427b2aa8944a1fcc8aeee11f2e633
Nahuel Gómez <nahuelgomez329@gmail.com>
2024-12-28 23:28:12 -0300

    sec_detect: sysfs support
    Report the currently running device at /sys/kernel/sec_detect/device_name.
    
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

--- drivers/misc/sec_detect.c
+++ drivers/misc/sec_detect.c
@@ -18,8 +18,13 @@
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/string.h>
+#ifdef CONFIG_SEC_DETECT_SYSFS
+#include <linux/kobject.h>
+#include <linux/sysfs.h>
+#endif
 
 int sec_current_device = DEVICE_UNKNOWN;
+char sec_current_device_name[32] = "Unknown";
 bool sec_needs_decon;
 bool sec_needs_blic = false;
 bool sec_doze = false; // Uses Samsung DRM Doze?
@@ -28,9 +33,31 @@ EXPORT_SYMBOL(sec_current_device);
 EXPORT_SYMBOL(sec_needs_blic);
 EXPORT_SYMBOL(sec_doze);
 
+#ifdef CONFIG_SEC_DETECT_SYSFS
+// Sysfs attribute to show the current device name
+static ssize_t device_name_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+    return snprintf(buf, 32, "%s\n", sec_current_device_name);
+}
+
+static struct kobj_attribute device_name_attr = __ATTR(device_name, 0444, device_name_show, NULL);
+
+static struct attribute *attrs[] = {
+    &device_name_attr.attr,
+    NULL,
+};
+
+static struct attribute_group attr_group = {
+    .attrs = attrs,
+};
+
+static struct kobject *device_kobj;
+#endif
+
 int sec_detect_init(void) {
     struct device_node *root;
     const char *machine_name;
+    int retval = 0;
 
     root = of_find_node_by_path("/");
     if (!root) {
@@ -51,31 +78,52 @@ int sec_detect_init(void) {
 
     if (strstr(machine_name, "A25") != NULL) {
         sec_current_device = SEC_A25;
+        strncpy(sec_current_device_name, "a25x", sizeof(sec_current_device_name));
         sec_needs_decon = false;
         sec_doze = true;
     } else if (strstr(machine_name, "A33") != NULL) {
         sec_current_device = SEC_A33;
+        strncpy(sec_current_device_name, "a33x", sizeof(sec_current_device_name));
         sec_needs_decon = true;
     } else if (strstr(machine_name, "A53") != NULL) {
         sec_current_device = SEC_A53;
+        strncpy(sec_current_device_name, "a53x", sizeof(sec_current_device_name));
         sec_needs_decon = true;
         sec_doze = true;
     } else if (strstr(machine_name, "M33") != NULL) {
         sec_current_device = SEC_M33;
+        strncpy(sec_current_device_name, "m33x", sizeof(sec_current_device_name));
         sec_needs_decon = true;
     } else if (strstr(machine_name, "M34") != NULL) {
         sec_current_device = SEC_M34;
+        strncpy(sec_current_device_name, "m34x", sizeof(sec_current_device_name));
         sec_needs_decon = false;
         sec_doze = true;
     } else if (strstr(machine_name, "GTA4XLS") != NULL) {
         sec_current_device = SEC_GTA4XLS;
+        strncpy(sec_current_device_name, "gta4xls", sizeof(sec_current_device_name));
         sec_needs_decon = false;
         sec_needs_blic = true;
     }
-    return 0;
+
+#ifdef CONFIG_SEC_DETECT_SYSFS
+    // Create the sysfs entry
+    device_kobj = kobject_create_and_add("sec_detect", kernel_kobj);
+    if (!device_kobj)
+        return -ENOMEM;
+
+    retval = sysfs_create_group(device_kobj, &attr_group);
+    if (retval)
+        kobject_put(device_kobj);
+#endif
+
+    return retval;
 }
 
 void sec_detect_exit(void) {
+#ifdef CONFIG_SEC_DETECT_SYSFS
+    kobject_put(device_kobj);
+#endif
     return;
 }
 
@@ -84,4 +132,4 @@ module_exit(sec_detect_exit);
 
 MODULE_AUTHOR("Flopster101");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Detects the Samsung device currently running this kernel.");
+MODULE_DESCRIPTION("Detects the Samsung device currently running this kernel. Also exposes it in /sys/kernel/sec_detect/device_name.");
--- include/linux/sec_detect.h
+++ include/linux/sec_detect.h
@@ -15,23 +15,30 @@
 
 #ifndef _LINUX_SEC_H
 #define _LINUX_SEC_H
-#define SEC_DETECT_LOG(fmt, ...) printk(KERN_INFO "sec_detect: " fmt, ##__VA_ARGS__)
 
 #include <linux/types.h>
+
+#define SEC_DETECT_LOG(fmt, ...) printk(KERN_INFO "sec_detect: " fmt, ##__VA_ARGS__)
 static const char *sec_detect_label = "sec_detect: ";
 
 enum SEC_devices {
-    DEVICE_UNKNOWN = -1,
-    SEC_A25,
-    SEC_A33,
-    SEC_A53,
-    SEC_M33,
-    SEC_M34,
-    SEC_GTA4XLS
+	DEVICE_UNKNOWN = -1,
+	SEC_A25,
+	SEC_A33,
+	SEC_A53,
+	SEC_M33,
+	SEC_M34,
+	SEC_GTA4XLS
 };
+
 extern enum SEC_devices sec_devices;
 extern int sec_current_device;
 extern bool sec_needs_decon;
 extern bool sec_needs_blic;
 extern bool sec_doze;
+
+#ifdef CONFIG_SEC_DETECT_SYSFS
+extern char sec_current_device_name[32];
 #endif
+
+#endif /* _LINUX_SEC_H */
