2bfe98ff8226fd88aa1158e4a1ece5762d0cc66b
Nahuel Gómez <nahuelgomez329@gmail.com>
2025-01-07 21:21:09 -0300

    exynos: mcd_v2: make a common camera config and prepare for a different approach
    The old style of unification is wrong, as some of these macros are only supposed to be enabled for some devices.
    
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

--- drivers/media/platform/exynos/camera/vendor/mcd_v2/aavw_common/is-vendor-config_aavw_common.h
+++ drivers/media/platform/exynos/camera/vendor/mcd_v2/aavw_common/is-vendor-config_aavw_common.h
@@ -2,80 +2,23 @@
 #define IS_VENDOR_CONFIG_AAVW_COMMON_H
 
 /***** HW DEFENDANT DEFINE *****/
+
 #define VENDER_PATH
-
-#define USE_AP_PDAF						/* Support sensor PDAF SW Solution */
-
-#define USE_IMX258_13MP_FULL_SIZE
 
 #define USE_CAMERA_HEAP
 #ifdef USE_CAMERA_HEAP
-#define CAMERA_HEAP_NAME	"camera"
-#define CAMERA_HEAP_NAME_LEN	6
-#define CAMERA_HEAP_UNCACHED_NAME	"camera-uncached"
-#define CAMERA_HEAP_UNCACHED_NAME_LEN	15
+#define CAMERA_HEAP_NAME    "camera"
+#define CAMERA_HEAP_NAME_LEN    6
+#define CAMERA_HEAP_UNCACHED_NAME   "camera-uncached"
+#define CAMERA_HEAP_UNCACHED_NAME_LEN   15
 #endif
-
-#define USE_HI1336C_SETFILE
-#define CAMERA_UWIDE_DUALIZED
-#define USE_GW1P_SETFILE
 
 #define CONFIG_SEC_CAL_ENABLE
-#define IS_REAR_MAX_CAL_SIZE (0x4970)
-#define IS_FRONT_MAX_CAL_SIZE (0x3310)
-#define IS_REAR3_MAX_CAL_SIZE (0x1AE0)
-#define IS_REAR4_MAX_CAL_SIZE (0x1A50)
 
-#define REAR_ULTRA_WIDE_CAMERA   (SENSOR_POSITION_REAR3)    /* Supported Camera Type for rear ultra wide */
-#define REAR_MACRO_CAMERA        (SENSOR_POSITION_REAR4)    /* Supported Camera Type for rear ultra wide */
-
-#define CAMERA_REAR2
-#define CAMERA_REAR3
-#define CAMERA_REAR4
-
-#define CAMERA_REAR_DUAL_CAL
-
-#define APPLY_MIRROR_VERTICAL_FLIP			/*Flip register need to be set to avoid mirror/rotated preview */
-
-#define READ_DUAL_CAL_FIRMWARE_DATA
-
-#ifdef USE_KERNEL_VFS_READ_WRITE
-#define DUAL_CAL_DATA_PATH "/vendor/firmware/SetMultiCalInfo.bin"
-#else
-#define DUAL_CAL_DATA_PATH "/vendor/firmware/"
-#define DUAL_CAL_DATA_BIN_NAME "SetMultiCalInfo.bin"
-#endif
-
-#define DUAL_CAL_DATA_SIZE_DEFAULT (0x080C)
-
-#define CAMERA_REAR2_MODULEID
-#define CAMERA_REAR3_MODULEID
-#define CAMERA_REAR4_MODULEID
-#define WIDE_OIS_ROM_ID ROM_ID_REAR
-#define USE_OIS_GYRO_TDK_ICM_42632M
-#define RELAX_OIS_GYRO_OFFSET_SPEC
-#define SIMPLIFY_OIS_INIT
-
-#define CONFIG_SECURE_CAMERA_USE 1
-#define ENABLE_REMOSAIC_CAPTURE
-#define MODIFY_CAL_MAP_FOR_SWREMOSAIC_LIB
-
-#define USE_LEDS_FLASH_CHARGING_VOLTAGE_CONTROL /* Flash config */
-
+#define CAMERA_EEPROM_SUPPORT_FRONT
+#define CAMERA_STANDARD_CAL_ISP_VERSION 'E'
 #define USES_STANDARD_CAL_RELOAD
 #define USE_PERSISTENT_DEVICE_PROPERTIES_FOR_CAL /* For cal reload */
 
-/* To handle CSI error issue, disable the dual sync setting */
-#define DISABLE_DUAL_SYNC
-
-#define CAMERA_EEPROM_SUPPORT_FRONT
-#define CONFIG_CAMERA_EEPROM_DUALIZED
-#define CAMERA_CAL_VERSION_GC5035B 'Q'
-#define CAMERA_STANDARD_CAL_ISP_VERSION 'E'
-
-#define USE_CAMERA_ADAPTIVE_MIPI
-
-#define USE_OIS_HALL_DATA_FOR_VDIS
-#define CONFIG_CHECK_HW_VERSION_FOR_MCU_FW_UPLOAD
-#define FRONT_OTPROM_EEPROM
+#define CONFIG_SECURE_CAMERA_USE 1
 #endif /* IS_VENDOR_CONFIG_AAV_V53X_H */
