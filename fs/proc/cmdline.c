7158bf2805b85c067c2a9940fa77be4b1c7aceac
Nahuel Gómez <nahuelgomez329@gmail.com>
2025-01-19 21:26:01 -0300

    [HACK] proc: cmdline: patch panel drivers module parameters
    Bootloader passes parameters to a few of the panel/drm drivers, but they aren't read correctly because they've been renamed during the unification.
    We can append the correct suffix to the cmdline by intercepting it on cmdline_proc_show.
    
    The device most affected by this issue is m33x, where only the Tianma LCD was working properly, as the driver was not able to detect which panel the bootloader passed and defaulted to 00 (Tianma).
    
    This is a very hacky approach, but it seems to work!
    
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -4,8 +4,60 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
+#ifdef CONFIG_SEC_DETECT_CMDLINE_PATCH
+#include <linux/string.h> // Include for string manipulation functions
+#include <linux/sec_detect.h>
+
+#define BUF_SIZE 4096   // Define a buffer size
+
+static void cmdline_panel_patch(char *command_line)
+{
+	char buf[BUF_SIZE];
+	char *pos = buf;
+	char *token;
+	const char *delim = " ";
+	int offset = 0;
+
+	// Initialize an empty buffer
+	memset(buf, 0, BUF_SIZE);
+
+	// Copy the command_line to the buffer
+	strncpy(buf, command_line, BUF_SIZE - 1);
+
+	// Tokenize the buffer to process each entry
+	while ((token = strsep(&pos, delim)) != NULL) {
+		if (sec_needs_decon) {
+			if (strstr(token, "mcd-panel.") == token) {
+				offset += snprintf(command_line + offset, BUF_SIZE - offset, "mcd-panel-decon%s ", token + strlen("mcd-panel"));
+			} else if (strstr(token, "exynos-drm.") == token) {
+				offset += snprintf(command_line + offset, BUF_SIZE - offset, "exynos-drm-decon%s ", token + strlen("exynos-drm"));
+			} else if (strstr(token, "mcd-panel-samsung-drv.") == token) {
+				offset += snprintf(command_line + offset, BUF_SIZE - offset, "mcd-panel-samsung-drv-decon%s ", token + strlen("mcd-panel-samsung-drv"));
+			} else {
+				offset += snprintf(command_line + offset, BUF_SIZE - offset, "%s ", token);
+			}
+		} else {
+			if (strstr(token, "mcd-panel-samsung-drv.") == token) {
+				offset += snprintf(command_line + offset, BUF_SIZE - offset, "mcd-panel-samsung-drv-usdm%s ", token + strlen("mcd-panel-samsung-drv"));
+			} else {
+				offset += snprintf(command_line + offset, BUF_SIZE - offset, "%s ", token);
+			}
+		}
+	}
+
+	// Remove the trailing space
+	if (offset > 0 && command_line[offset - 1] == ' ')
+		command_line[offset - 1] = '\0';
+}
+#endif
+
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
+#ifdef CONFIG_SEC_DETECT_CMDLINE_PATCH
+	// Modify the saved_command_line before showing it
+	cmdline_panel_patch(saved_command_line);
+#endif
+
 	seq_puts(m, saved_command_line);
 	seq_putc(m, '\n');
 	return 0;
