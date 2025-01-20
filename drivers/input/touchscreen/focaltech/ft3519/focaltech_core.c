3822e41b29d84c661df59c8fdbf7ab74b1910e7f
Nahuel Gómez <nahuelgomez329@gmail.com>
2025-01-20 17:37:40 -0300

    input: focaltech: refactor i2c probe/remove functions
    * For the newer focaltech driver, using sec_input from a25x.
    
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

--- drivers/input/touchscreen/focaltech/ft3519/focaltech_core.c
+++ drivers/input/touchscreen/focaltech/ft3519/focaltech_core.c
@@ -2120,33 +2120,30 @@ static int fts_ts_probe_entry(struct fts_ts_data *ts_data)
 
 	FTS_FUNC_ENTER();
 	FTS_INFO("%s", FTS_DRIVER_VERSION);
+
 	ts_data->pdata = devm_kzalloc(ts_data->dev, pdata_size, GFP_KERNEL);
-	if (!ts_data->pdata) {
-		FTS_ERROR("allocate memory for platform_data fail");
+	if (!ts_data->pdata)
 		return -ENOMEM;
-	}
 
 	ts_data->dev->platform_data = ts_data->pdata;
 
 	if (ts_data->dev->of_node) {
 		ret = sec_input_parse_dt(ts_data->dev);
-		//ret = fts_parse_dt(ts_data->dev, ts_data->pdata);
 		if (ret) {
 			FTS_ERROR("device-tree parse fail");
-			goto err_parse_dt;
+			return ret;
 		}
+	} else if (ts_data->dev->platform_data) {
+		memcpy(ts_data->pdata, ts_data->dev->platform_data, pdata_size);
 	} else {
-		if (ts_data->dev->platform_data) {
-			memcpy(ts_data->pdata, ts_data->dev->platform_data, pdata_size);
-		} else {
-			FTS_ERROR("platform_data is null");
-			goto err_parse_dt;
-		}
+		FTS_ERROR("platform_data is null");
+		return -EINVAL;
 	}
 
 	ts_data->ts_workqueue = create_singlethread_workqueue("fts_wq");
 	if (!ts_data->ts_workqueue) {
 		FTS_ERROR("create fts workqueue fail");
+		return -ENOMEM; // Return error on workqueue creation failure
 	}
 
 	mutex_init(&ts_data->report_mutex);
@@ -2154,24 +2151,16 @@ static int fts_ts_probe_entry(struct fts_ts_data *ts_data)
 	mutex_init(&ts_data->device_lock);
 	mutex_init(&ts_data->irq_lock);
 
-	/* Init communication interface */
 	ret = fts_bus_init(ts_data);
 	if (ret) {
 		FTS_ERROR("bus initialize fail");
-		goto err_bus_init;
+		goto err_mutex_destroy;
 	}
 
-/*
-	ret = fts_input_init(ts_data);
-	if (ret) {
-		FTS_ERROR("input initialize fail");
-		goto err_input_init;
-	}
-*/
 	ret = sec_input_device_register(ts_data->dev, ts_data);
 	if (ret) {
 		FTS_ERROR("failed to register input device, %d", ret);
-		goto err_input_init;
+		goto err_input_unregister;
 	}
 	ts_data->input_dev = ts_data->pdata->input_dev;
 	ts_data->input_dev_proximity = ts_data->pdata->input_dev_proximity;
@@ -2187,16 +2176,9 @@ static int fts_ts_probe_entry(struct fts_ts_data *ts_data)
 	ret = fts_report_buffer_init(ts_data);
 	if (ret) {
 		FTS_ERROR("report buffer init fail");
-		goto err_report_buffer;
+		goto err_input_unregister;
 	}
 
-/*
-	ret = fts_gpio_configure(ts_data);
-	if (ret) {
-		FTS_ERROR("configure the gpios fail");
-		goto err_gpio_config;
-	}
-*/
 #if FTS_POWER_SOURCE_CUST_EN
 	ret = fts_power_source_init(ts_data);
 #else
@@ -2204,13 +2186,13 @@ static int fts_ts_probe_entry(struct fts_ts_data *ts_data)
 #endif
 	if (ret) {
 		FTS_ERROR("fail to get power(regulator)");
-		goto err_power_init;
+		goto err_input_unregister;
 	}
 
 	ret = fts_get_ic_information(ts_data);
 	if (ret) {
 		FTS_ERROR("not focal IC, unregister driver");
-		goto err_irq_req;
+		goto err_power_off;
 	}
 
 #if !IS_ENABLED(CONFIG_SAMSUNG_PRODUCT_SHIP)
@@ -2252,13 +2234,13 @@ static int fts_ts_probe_entry(struct fts_ts_data *ts_data)
 	ret = fts_irq_registration(ts_data);
 	if (ret) {
 		FTS_ERROR("request irq failed");
-		goto err_irq_req;
+		goto err_power_off;
 	}
 
 	ret = fts_fwupg_init(ts_data);
 	if (ret) {
 		FTS_ERROR("init fw upgrade fail");
-		goto err_fwupg_init;
+		goto err_irq_free;
 	}
 
 	atomic_set(&ts_data->pdata->enabled, true);
@@ -2269,7 +2251,7 @@ static int fts_ts_probe_entry(struct fts_ts_data *ts_data)
 	ret = fts_sec_cmd_init(ts_data);
 	if (ret) {
 		FTS_ERROR("init sec_cmd fail");
-		goto err_cmd_init;
+		goto err_fwupg_free;
 	}
 
 #if IS_ENABLED(CONFIG_VBUS_NOTIFIER)
@@ -2294,30 +2276,32 @@ static int fts_ts_probe_entry(struct fts_ts_data *ts_data)
 	FTS_FUNC_EXIT();
 	return 0;
 
-err_cmd_init:
+err_fwupg_free:
 	ts_data->pdata->enable = NULL;
 	ts_data->pdata->disable = NULL;
-err_fwupg_init:
+	fts_fwupg_exit(ts_data);
+err_irq_free:
 	fts_irq_disable();
 	free_irq(ts_data->irq, ts_data);
-err_irq_req:
-err_power_init:
+err_power_off:
 #if FTS_POWER_SOURCE_CUST_EN
 	fts_power_source_exit(ts_data);
+#else
+	fts_ts_power_off(ts_data);
 #endif
-	if (gpio_is_valid(ts_data->pdata->irq_gpio))
-		gpio_free(ts_data->pdata->irq_gpio);
-err_report_buffer:
-err_input_init:
-	if (ts_data->ts_workqueue)
-		destroy_workqueue(ts_data->ts_workqueue);
-err_bus_init:
+err_input_unregister:
+	if (ts_data->input_dev)
+		input_unregister_device(ts_data->input_dev);
+	if (ts_data->input_dev_proximity)
+		input_unregister_device(ts_data->input_dev_proximity);
+err_mutex_destroy:
 	mutex_destroy(&ts_data->report_mutex);
 	mutex_destroy(&ts_data->bus_lock);
 	mutex_destroy(&ts_data->device_lock);
 	mutex_destroy(&ts_data->irq_lock);
-err_parse_dt:
-	FTS_FUNC_EXIT();
+	if (ts_data->ts_workqueue) {
+		destroy_workqueue(ts_data->ts_workqueue);
+	}
 	return ret;
 }
 
@@ -2329,7 +2313,27 @@ static int fts_ts_remove_entry(struct fts_ts_data *ts_data)
 	ts_data->pdata->disable = NULL;
 	atomic_set(&ts_data->pdata->shutdown_called, true);
 
-	disable_irq(ts_data->irq);
+	if (!ts_data) {
+		FTS_ERROR("ts_data is NULL");
+		return -EINVAL;
+	}
+
+	if (ts_data->input_dev) {
+		input_unregister_device(ts_data->input_dev);
+		ts_data->input_dev = NULL;
+	}
+
+	if (ts_data->input_dev_proximity) {
+		input_unregister_device(ts_data->input_dev_proximity);
+		ts_data->input_dev_proximity = NULL;
+	}
+
+	if (ts_data->irq) { // Check if IRQ was requested
+		disable_irq(ts_data->irq);
+		free_irq(ts_data->irq, ts_data);
+		ts_data->irq = 0; // Reset client irq
+	}
+
 	cancel_delayed_work_sync(&ts_data->print_info_work);
 	cancel_delayed_work_sync(&ts_data->read_info_work);
 
@@ -2347,8 +2351,8 @@ static int fts_ts_remove_entry(struct fts_ts_data *ts_data)
 	fts_release_apk_debug_channel(ts_data);
 	fts_remove_sysfs(ts_data);
 #endif
-	fts_ex_mode_exit(ts_data);
 
+	fts_ex_mode_exit(ts_data);
 	fts_fwupg_exit(ts_data);
 
 #if FTS_ESDCHECK_EN
@@ -2360,24 +2364,21 @@ static int fts_ts_remove_entry(struct fts_ts_data *ts_data)
 
 	fts_wakeup_source_unregister(ts_data);
 
-	free_irq(ts_data->irq, ts_data);
-
-	if (ts_data->ts_workqueue)
+	if (ts_data->ts_workqueue) {
 		destroy_workqueue(ts_data->ts_workqueue);
-
-	if (gpio_is_valid(ts_data->pdata->irq_gpio))
-		gpio_free(ts_data->pdata->irq_gpio);
+		ts_data->ts_workqueue = NULL;
+	}
 
 #if FTS_POWER_SOURCE_CUST_EN
 	fts_power_source_exit(ts_data);
 #endif
+
 	mutex_destroy(&ts_data->report_mutex);
 	mutex_destroy(&ts_data->bus_lock);
 	mutex_destroy(&ts_data->device_lock);
 	mutex_destroy(&ts_data->irq_lock);
 
 	FTS_FUNC_EXIT();
-	ts_data = NULL; // Set pointer to NULL to avoid use-after-free
 
 	return 0;
 }
