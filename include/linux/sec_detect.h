584298069cacbbd6c481589c33f07e02505a7f77
Nahuel Gómez <nahuelgomez329@gmail.com>
2025-02-09 16:21:57 -0300

    sec_detect: move to SPDX license identifier
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

--- include/linux/sec_detect.h
+++ include/linux/sec_detect.h
@@ -1,10 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  * Author: @Flopster101
  * Based on AkiraNoSushi's work for the Mi439 project.
- *
- * This software is licensed under the terms of the GNU General Public
- * License version 2, as published by the Free Software Foundation, and
- * may be copied, distributed, and modified under those terms.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
