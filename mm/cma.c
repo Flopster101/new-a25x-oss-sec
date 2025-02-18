c1b379f16c7fe0437fcc59ba3609a64646d4b9fa
Nahuel Gómez <nahuelgomez329@gmail.com>
2025-02-18 19:15:48 -0300

    [EXPERIMENTAL] mm: Import A256EXXU7BXL1 OSRC changes
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

--- mm/cma.c
+++ mm/cma.c
@@ -393,7 +393,6 @@ err:
 	return ret;
 }
 
-#ifdef CONFIG_CMA_DEBUG
 static void cma_debug_show_areas(struct cma *cma)
 {
 	unsigned long next_zero_bit, next_set_bit, nr_zero;
@@ -418,9 +417,6 @@ static void cma_debug_show_areas(struct cma *cma)
 	pr_cont("=> %lu free of %lu total pages\n", nr_total, cma->count);
 	mutex_unlock(&cma->lock);
 }
-#else
-static inline void cma_debug_show_areas(struct cma *cma) { }
-#endif
 
 /**
  * cma_alloc() - allocate pages from contiguous area
@@ -443,7 +439,7 @@ struct page *cma_alloc(struct cma *cma, size_t count, unsigned int align,
 	struct page *page = NULL;
 	int ret = -ENOMEM;
 	int num_attempts = 0;
-	int max_retries = 5;
+	int max_retries = 10;
 	s64 ts;
 	struct cma_alloc_info cma_info = {0};
 
