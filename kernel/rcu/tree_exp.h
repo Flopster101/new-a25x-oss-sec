d6c47e3de739720397388219f38304624603b1a4
Nahuel Gómez <nahuelgomez329@gmail.com>
2025-02-18 19:11:43 -0300

    kernel: Import A256EXXU7BXL1 OSRC changes
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

--- kernel/rcu/tree_exp.h
+++ kernel/rcu/tree_exp.h
@@ -8,6 +8,7 @@
  */
 
 #include <linux/lockdep.h>
+#include <linux/sec_debug.h>
 
 static void rcu_exp_handler(void *unused);
 static int rcu_print_task_exp_stall(struct rcu_node *rnp);
@@ -623,7 +624,7 @@ static void synchronize_rcu_expedited_wait(void)
 			continue;
 		panic_on_rcu_stall();
 		trace_rcu_stall_warning(rcu_state.name, TPS("ExpeditedStall"));
-		pr_err("INFO: %s detected expedited stalls on CPUs/tasks: {",
+		pr_auto(ASL1, "INFO: %s detected expedited stalls on CPUs/tasks: {",
 		       rcu_state.name);
 		ndetected = 0;
 		rcu_for_each_leaf_node(rnp) {
@@ -670,6 +671,9 @@ static void synchronize_rcu_expedited_wait(void)
 				preempt_enable();
 			}
 		}
+		if (IS_ENABLED(CONFIG_SEC_DEBUG_PANIC_ON_RCU_STALL))
+			panic("RCU Stall\n");
+
 		jiffies_stall = 3 * rcu_jiffies_till_stall_check() + 3;
 	}
 }
@@ -939,6 +943,7 @@ void synchronize_rcu_expedited(void)
 		/* Marshall arguments & schedule the expedited grace period. */
 		rew.rew_s = s;
 		synchronize_rcu_expedited_queue_work(&rew);
+		secdbg_dtsk_built_set_data(DTYPE_WORK, &rew.rew_work);
 	}
 
 	/* Wait for expedited grace period to complete. */
@@ -946,6 +951,8 @@ void synchronize_rcu_expedited(void)
 	wait_event(rnp->exp_wq[rcu_seq_ctr(s) & 0x3],
 		   sync_exp_work_done(s));
 	smp_mb(); /* Work actions happen before return. */
+
+	secdbg_dtsk_built_clear_data();
 
 	/* Let the next expedited grace period start. */
 	mutex_unlock(&rcu_state.exp_mutex);
